17817c403527079f4f87cde7ac076416f7141fc7
87923praveen <hosamanipraveen332@gmail.com>
2026-01-20 14:13:15 +0530

    update files

--- src/Teacher.java
+++ src/Teacher.java
@@ -40,5 +40,40 @@ public class Teacher {
             System.out.println("fail");
         }
 
+        //Switch statement
+        int day = 2;
+        switch (day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+
+            case 2:
+                System.out.println("Monday");
+                break;
+
+            case 3:
+                System.out.println("Tuesday");
+                break;
+
+            case 4:
+                System.out.println("Wednesday");
+                break;
+
+            case 5:
+                System.out.println("Thursday");
+                break;
+
+            case 6:
+                System.out.println("Friday");
+                break;
+
+            case 7:
+                System.out.println("Saturday");
+                break;
+
+            default:
+            System.out.println("Invalid Day");
+
+        }
     }
 }
